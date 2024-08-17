bcfcc6ba7285d6c74224072de4e61b5b89e7d736
Joshua Lee <jminlee24@outlook.com>
2024-08-17 13:54:17 -0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import appCss from "./App.module.css";
+import Header from "./components/header/header.tsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className={appCss.header}>
+        <Header />
+      </div>
+      <div>
+        <div>Hello!</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
