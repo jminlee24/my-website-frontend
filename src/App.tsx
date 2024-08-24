38ab69b0ea441cf9634a579a8cf180526bbe7255
jminlee24 <jminlee24@outlook.com>
2024-08-24 18:35:53 -0400

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import appCss from "./App.module.css";
 import Header from "./components/header/header.tsx";
+import TitleScreen from "./components/titleScreen/titleScreen.tsx";
+import BackgroundCanvas from "./components/backgroundCanvas/backgroundCanvas.tsx";
 
 function App() {
   return (
-    <>
+    <div className={appCss.main_container}>
       <div className={appCss.header}>
         <Header />
       </div>
-      <div>
-        <div>Hello!</div>
+      <div id="canvas-container" className={appCss.canvas_container}>
+        <BackgroundCanvas />
       </div>
-    </>
+      <div>
+        <TitleScreen />
+      </div>
+    </div>
   );
 }
 
