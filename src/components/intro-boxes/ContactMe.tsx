33e241ed89e1a948946e798144556a61dac6403f
jminlee24 <jminlee24@outlook.com>
2024-11-17 02:00:24 -0500

    added resume + contacts

--- src/components/intro-boxes/ContactMe.tsx
+++ src/components/intro-boxes/ContactMe.tsx
@@ -1,4 +1,5 @@
 import { FaLink } from "react-icons/fa";
+import resume from "../../assets/resume.pdf";
 
 const ContactMe = () => {
   return (
@@ -10,18 +11,42 @@ const ContactMe = () => {
         </p>
         <br></br>
         <h2 className="p-5 text-bold text-lg font-bold">Socials:</h2>
-        <ul className="flex flex-col px-5 gap-1">
-          <li>Instagram</li>
-          <li>LinkedIn</li>
-          <li>Github</li>
-          <li>Discord</li>
+        <ul className="flex flex-col px-5 gap-1 ">
+          <a
+            className="hover:font-bold"
+            href="https://github.com/jminlee24"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            className="hover:font-bold"
+            href="https://www.linkedin.com/in/jmlee2004/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="hover:font-bold"
+            href="https://www.instagram.com/josh.m.lee/"
+            target="_blank"
+          >
+            Instagram
+          </a>
+          <a
+            className="hover:font-bold"
+            href="https://discordapp.com/users/368149745387962378"
+            target="_blank"
+          >
+            Discord
+          </a>
         </ul>
       </div>
       <div className="p-5 flex flex-row align-middle">
-        <div className="flex flex-row gap-2">
+        <a href={resume} target="_blank" className="flex flex-row gap-2">
           <h2 className="py-5 text-bold text-xl font-bold ">Resume</h2>
           <FaLink className="align-middle self-center" />
-        </div>
+        </a>
       </div>
     </div>
   );
