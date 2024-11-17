49af876a2d74a01b8acd2360087d9e32243f636b
jminlee24 <jminlee24@outlook.com>
2024-11-17 01:35:55 -0500

    animation and colors

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,44 +1,89 @@
+import { motion} from "motion/react";
+
+import { useState } from "react";
 import AboutMe from "./intro-boxes/AboutMe";
+import ColorTheme from "./intro-boxes/ColorTheme";
+import ContactMe from "./intro-boxes/ContactMe";
+import FunFact from "./intro-boxes/FunFact";
 import MainIntro from "./intro-boxes/MainIntro";
+import Projects from "./intro-boxes/Projects";
 
-type Props = {};
+const MainContainer = () => {
+  const [color, setColor] = useState<string>("white");
 
-const MainContainer = (props: Props) => {
+  const handleColorChange = (color: string) => {
+    setColor(color);
+    console.log(color);
+  };
+
   return (
-    <main
+    <motion.main
       className={`
         lg:w-11/12 w-full h-full align-middle p-12 pb-10 m-auto
-          outline overflow-hidden relative
-        grid lg:grid-cols-4 lg:grid-rows-6 lg:gap-4
-        md:grid-cols-3 md:gap-2
-        sm:grid-cols-1 sm:gap-1
+        outline relative
+        lg:overflow-y-hidden
+        overflow-y-auto 
+        overflow-x-hidden
+        grid lg:grid-cols-4 lg:grid-rows-6 gap-4
+        md:grid-cols-3
+        sm:grid-cols-1 
+        ${color}
 
         `}
+      initial="hidden"
+      whileInView="visible"
+      variants={{
+        visible: {
+          opacity: 1,
+          transition: { when: "beforeChildren", staggerChildren: 0.1 },
+        },
+        hidden: { opacity: 0 },
+      }}
+      animate={{ x: 0 }}
     >
-      <div className="border rounded-xl border-white md:col-span-3 lg:row-span-2 bg-gray-800">
+      <motion.div
+        variants={{ visible: { x: 0 }, hidden: { x: -1000 } }}
+        className="border rounded-xl hover:border-outline border-gray-600 md:col-span-3 lg:row-span-2 bg-gray-800"
+      >
         <MainIntro />
-      </div>
-      <div className="border rounded-xl border-white md:col-span-1 md:row-span-4 bg-gray-800">
+      </motion.div>
+      <motion.div
+        variants={{ visible: { x: 0 }, hidden: { x: 1000 } }}
+        className="border rounded-xl hover:border-outline border-gray-600 md:col-span-1 md:row-span-4 bg-gray-800"
+      >
         <AboutMe />
-      </div>
-      <div className="border rounded-xl border-white md:col-span-2 lg:row-span-2 bg-gray-800"></div>
-
-      <div className="border rounded-xl border-white md:col-span-1 lg:row-span-3 bg-gray-800">
-        Contact me
-      </div>
-      <div className="border rounded-xl border-white md:col-span-1 lg:row-span-2 bg-gray-800">
-        Fun fact
-      </div>
-      <div className="border rounded-xl border-white md:col-span-1 lg:row-span-2 bg-gray-800">
+      </motion.div>
+      <motion.div
+        variants={{ visible: { x: 0 }, hidden: { x: -1000 } }}
+        className="border rounded-xl hover:border-outline border-gray-600 md:col-span-2 lg:row-span-2 bg-gray-800"
+      >
+        <Projects />
+      </motion.div>
+      <motion.div
+        variants={{ visible: { y: 0 }, hidden: { y: 1000 } }}
+        className="border rounded-xl hover:border-outline border-gray-600 md:col-span-1 lg:row-span-3 bg-gray-800"
+      >
+        <ContactMe />
+      </motion.div>
+      <motion.div
+        variants={{ visible: { x: 0 }, hidden: { x: -1000 } }}
+        className="border rounded-xl hover:border-outline border-gray-600 md:col-span-1 lg:row-span-2 bg-gray-800"
+      >
+        <FunFact />
+      </motion.div>
+      <motion.div
+        variants={{ visible: { y: 0 }, hidden: { y: 1000 } }}
+        className="border rounded-xl hover:border-outline border-gray-600 md:col-span-1 lg:row-span-2 bg-gray-800"
+      >
         OTHER COOL THING
-      </div>
-      <div className="border rounded-xl border-white md:col-span-1 lg:row-span-1 bg-gray-800">
-        RESUME
-      </div>
-      <div className="border rounded-xl border-white md:col-span-2 lg:row-span-1 bg-gray-800">
-        COOL THING
-      </div>
-    </main>
+      </motion.div>
+      <motion.div
+        variants={{ visible: { x: 0 }, hidden: { x: 1000 } }}
+        className="border rounded-xl hover:border-outline border-gray-600 md:col-span-1 lg:row-span-1 bg-gray-800"
+      >
+        <ColorTheme color={color} handleColorChange={handleColorChange} />
+      </motion.div>
+    </motion.main>
   );
 };
 
