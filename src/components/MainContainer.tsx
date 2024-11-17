9db8afac56cee2d75f76ca94d60911ef67243df2
jminlee24 <jminlee24@outlook.com>
2024-11-16 20:48:57 -0500

    increase rounded

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -15,28 +15,27 @@ const MainContainer = (props: Props) => {
 
         `}
     >
-      <div className="border rounded-lg border-white md:col-span-3 lg:row-span-2 bg-gray-800">
+      <div className="border rounded-xl border-white md:col-span-3 lg:row-span-2 bg-gray-800">
         <MainIntro />
       </div>
-      <div className="border rounded-lg border-white md:col-span-1 md:row-span-4 bg-gray-800">
+      <div className="border rounded-xl border-white md:col-span-1 md:row-span-4 bg-gray-800">
         <AboutMe />
       </div>
-      <div className="border rounded-lg border-white md:col-span-2 lg:row-span-2">
-        Highlighted Project
-      </div>
-      <div className="border rounded-lg border-white md:col-span-1 lg:row-span-3">
+      <div className="border rounded-xl border-white md:col-span-2 lg:row-span-2 bg-gray-800"></div>
+
+      <div className="border rounded-xl border-white md:col-span-1 lg:row-span-3 bg-gray-800">
         Contact me
       </div>
-      <div className="border rounded-lg border-white md:col-span-1 lg:row-span-2">
+      <div className="border rounded-xl border-white md:col-span-1 lg:row-span-2 bg-gray-800">
         Fun fact
       </div>
-      <div className="border rounded-lg border-white md:col-span-1 lg:row-span-2">
+      <div className="border rounded-xl border-white md:col-span-1 lg:row-span-2 bg-gray-800">
         OTHER COOL THING
       </div>
-      <div className="border rounded-lg border-white md:col-span-1 lg:row-span-1">
+      <div className="border rounded-xl border-white md:col-span-1 lg:row-span-1 bg-gray-800">
         RESUME
       </div>
-      <div className="border rounded-lg border-white md:col-span-2 lg:row-span-1">
+      <div className="border rounded-xl border-white md:col-span-2 lg:row-span-1 bg-gray-800">
         COOL THING
       </div>
     </main>
